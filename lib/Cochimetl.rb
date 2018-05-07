c892b787b8a7f8ec50fd8c40302f38898c662dd4
Jesus Hernández <jahl-v2@hotmail.com>
2018-05-07 14:25:26 -0500

    chg: added functionality to the main module

--- lib/Cochimetl.rb
+++ lib/Cochimetl.rb
@@ -1,7 +1,15 @@
-require "Cochimetl/version"
-require 'nokogiri'
+require "Cochimetl"
 require 'savon'
+require 'nokogiri'
 
 module Cochimetl
-  # Your code goes here...
+  @@client = Cochimetl::WebServiceClient.new
+
+  def self.client
+    @@client
+  end
+
+  def self.new(value, currency)
+    Cochimetl::ExchangeRate.new(value, currency)
+  end
 end
